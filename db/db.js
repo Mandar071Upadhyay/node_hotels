b6a5ee5d6376cf525181cdf0c518bb054fd6e6c7
Mandar Upadhyay <mandar.upadhyay12345@gmail.com>
2024-09-03 09:45:17 +0530

    move to online server

--- db/db.js
+++ db/db.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-const mongodbURL = "mongodb://localhost:27017/hotels";
+require("dotenv").config();
+//const mongodbURL = "mongodb://localhost:27017/hotels";
+//const mongodbURL="mongodb+srv://mandarupadhyay12345:xVrzzD3aUHScn5Qk@cluster0.c2z98.mongodb.net/hotels?retryWrites=true&w=majority&appName=Cluster0";
+const mongodbURL=process.env.DB_URL;
+
 mongoose.connect(mongodbURL);
 const db = mongoose.connection;
 db.on("connected", () => {
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express=require('express');
 const db=require('./db/db');
 const app=express();
 const bodyParser=require('body-parser');
-const cron = require('node-cron');
+require("dotenv").config();
+
 
 app.use(bodyParser.json());
 // import router
@@ -12,6 +13,9 @@ const personRouter=require('./routers/personRouter');
 //user middle were for router
 app.use('/person',personRouter);
 
-app.listen(3000,(error)=>{
+
+//port variable
+const port=process.env.PORT;
+app.listen(port,(error)=>{
 console.log(`server is ready to accept request on port 3000`);
 })
